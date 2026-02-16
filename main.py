2cc7fb18eead640db5c83a8d742657ab037963fe
bhatiwalk <83658232+bhatiwalk@users.noreply.github.com>
2026-02-16 15:27:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -44,6 +44,13 @@ async def predict(file: UploadFile = File(...)):
         "detections": detections
     })
 
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
 #uvicorn main:app --reload --host 0.0.0.0 --port 8000
 
